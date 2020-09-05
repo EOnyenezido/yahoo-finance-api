6e966fb928a8c8f4ec00e80194920318792385c2
Victor Onyenezido <sleonvictor@gmail.com>
2020-09-05 14:25:22 +0100

    Add get price route and region validation

--- yfinance.py
+++ yfinance.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from config import supportedRegions
 
 app = Flask(__name__)
+
+@app.route('/stock/v1/get-price')
+def get_price():
+    region = request.args.get('region', '').upper()
+    if region not in supportedRegions:
+        return {
+            "success": False,
+            "message": "Unsupported region. Please specify one of " + ",".join(supportedRegions)
+        }, 400
+
+    return {} # return an empty JSON until integration with Yahoo Finance API
 
 @app.errorhandler(404)
 def route_not_found(error):
