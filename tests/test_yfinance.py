31ab6f119146b39e5988ae956e9b71a8bc8c07ef
Victor Onyenezido <sleonvictor@gmail.com>
2020-09-05 14:22:48 +0100

    Add tests for region parameter and unsupported regions
    This commit adds the below two tests
    - Test to make sure the region parameter is in the request url
    - Test to make sure the region is a supported region

--- tests/test_yfinance.py
+++ tests/test_yfinance.py
@@ -1,6 +1,7 @@
 import unittest
 
 from yfinance import app # pylint: disable=F0401
+from config import supportedRegions # pylint: disable=F0401
 
 class TestYFinance(unittest.TestCase):
     def setUp(self):
@@ -40,3 +41,29 @@ class TestYFinance(unittest.TestCase):
         body = rv.get_json()
         self.assertEqual(body["success"], False)
         self.assertIn("URL does not exist", body["message"])
+
+    def test_region_must_be_in_request_url(self):
+        """
+        GIVEN a url without region paramater
+        WHEN a request is made
+        THEN should return bad request error
+        """
+        rv = self.client.get("/stock/v1/get-price?symbol=AMRN")
+        self.assertTrue(rv.is_json)
+        self.assertEqual(rv.status_code, 400)
+        body = rv.get_json()
+        self.assertEqual(body["success"], False)
+        self.assertIn("Unsupported region", body["message"])
+
+    def test_region_must_be_supported_region(self):
+        """
+        GIVEN a url with an unsupported region paramater
+        WHEN a request is made
+        THEN should return bad request error including a list of supported regions
+        """
+        rv = self.client.get("/stock/v1/get-price?region=**&symbol=AMRN")
+        self.assertTrue(rv.is_json)
+        self.assertEqual(rv.status_code, 400)
+        body = rv.get_json()
+        self.assertEqual(body["success"], False)
+        self.assertEqual(body["message"], "Unsupported region. Please specify one of " + ",".join(supportedRegions))
