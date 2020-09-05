5911572bd5a851c1dfcc269b4a0f8d4a450c696a
Victor Onyenezido <sleonvictor@gmail.com>
2020-09-05 14:20:52 +0100

    Add a simple configuation file
    This commit adds this python file that will be used to store
    configuration items.
    
    I am using a simple python configuation file as the project
    configuration requirements as small

---
+++ config.py
@@ -0,0 +1,4 @@
+# Quick, simple configuration file
+# Using this because project configuration requirement is very small
+
+supportedRegions = ["AU", "CA", "FR", "DE", "HK", "US", "IT", "ES", "GB", "IN"]
