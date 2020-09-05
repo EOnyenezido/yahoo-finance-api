bee3ec5ac643ece441b6c62cd9886607767683d3
Victor Onyenezido <sleonvictor@gmail.com>
2020-09-05 16:04:13 +0100

    Add Yahoo Finance API url and authentication params
    Storing the API key here for brevity sake
    
    Production will move this key to an environment variable, database
    or secret config file

--- config.py
+++ config.py
@@ -2,3 +2,8 @@
 # Using this because project configuration requirement is very small
 
 supportedRegions = ["AU", "CA", "FR", "DE", "HK", "US", "IT", "ES", "GB", "IN"]
+
+url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary"
+
+rapidapi_host = "apidojo-yahoo-finance-v1.p.rapidapi.com"
+rapidapi_key = "bd0854be6amsh7fdeaedc50e17b6p175995jsnc554296a8f3f"
